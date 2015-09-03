1f6748997b4bd3ee08d97be2609ad2dd6e6ced53
Dana Scheider <dana.scheider@gmail.com>
2015-09-03 08:46:49 -0700

    Exercises about Boolean values

---
+++ booleans.py
@@ -0,0 +1,11 @@
+print "x     y     x and y"
+print True, True, True and True
+print True, False, True and False
+print False, True, False and True
+print False, False, False and False
+
+print "x    y    x or y"
+print True, True, True or True
+print True, False, True or False
+print False, True, False or True
+print False, False, False or False
