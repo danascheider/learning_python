57851a71d36d8daf8d77f4a111d54e7d4adff674
Dana Scheider <dana.scheider@gmail.com>
2015-09-07 17:17:18 -0700

    Add standard deviation function

--- building_skills_in_python/tuple_exercises/mean.py
+++ building_skills_in_python/tuple_exercises/mean.py
@@ -1,3 +1,5 @@
+import math
+
 # Computing the mean of a list of values is relatively simple. The mean is the sum of 
 # the values divided by the number of values in the list. Write a function that finds
 # the mean value in a tuple.
@@ -9,3 +11,17 @@ def mean(tup):
     s += num
 
   return float(s) / len(tup)
+
+# The standard deviation can be done a few ways, but we'll use the formula shown below. 
+# This computes a deviation measurement as the square of the difference between each 
+# sample and the mean. The sum of these measurements is then divided by the number of 
+# values times the number of degrees of freedom to get a standardized deviation measurement. 
+
+def sdev(tup):
+  xBar = mean(tup)
+  s    = 0
+
+  for num in tup:
+    s += (num - xBar) ** 2
+
+  return math.sqrt(float(s) / (len(tup) - 1))
